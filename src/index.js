a7bfd1c06e4b06b7592bb179376d67b9a6f7a894
Alexander Tzoc <jealtzo26@gmail.com>
2021-02-22 14:32:41 -0600

    Agregando boton para limpiar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from './lazy'
 
-console.log('Happy hacking :)')
+const baseUrl = 'https://randomfox.ca/images/'
+const appNode = document.querySelector('#images')
+//buttonAdd.addEventListener('click', addImage)
+
+document.querySelector('#utils').addEventListener('click', (event)=>{
+    if(event.target.nodeName === 'BUTTON'){
+        switch(event.target.id){
+            case 'buttonAdd':
+                addImage()
+                break
+            case 'buttonClear':
+                clearAll()
+                break
+        }
+    }
+})
+
+
+function clearAll(){
+    appNode.innerHTML = "";
+}
+
+function addImage() {
+    //Create image container
+    const container = document.createElement('div')
+    container.className = 'p-4'
+    //Create image
+    const image = document.createElement('img')
+    image.width = '320'
+    const number = random(122, 1)
+    image.dataset.src = `${baseUrl}${number}.jpg`
+    image.className = 'mx-auto'
+
+    //Add image into image container
+    container.appendChild(image)
+    appNode.appendChild(container)
+    registerImage(container)
+}
+
+const random = (max, min) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
