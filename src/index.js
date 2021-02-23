92afd397d30ff219f91efc0931196ef30c4af60e
Alexander Tzoc <jealtzo26@gmail.com>
2021-02-22 18:51:11 -0600

    Proyecto terminado

--- src/index.js
+++ src/index.js
@@ -25,18 +25,20 @@ function clearAll(){
 function addImage() {
     //Create image container
     const container = document.createElement('div')
-    container.className = 'p-4'
+    container.className = 'p-4'        
     //Create image
     const image = document.createElement('img')
     image.width = '320'
+    image.height = '350'
+    image.style.backgroundColor = '#000'
     const number = random(122, 1)
     image.dataset.src = `${baseUrl}${number}.jpg`
     image.className = 'mx-auto'
 
     //Add image into image container
     container.appendChild(image)
-    appNode.appendChild(container)
-    registerImage(container)
+    appNode.appendChild(container)    
+    registerImage(container, appNode.childElementCount)
 }
 
 const random = (max, min) => {
--- src/lazy.js
+++ src/lazy.js
@@ -1,20 +1,43 @@
+let reports
+
 const isIntersecting = (entry) => {
     return entry.isIntersecting
 }
 const loadImage = (entry) => {
     const container = entry.target
     const image = container.firstChild    
-    const url = image.dataset.src
-    image.src = url
+    const url = image.dataset.src    
+    image.src = url            
+    reports(0, 1)      
     observer.unobserve(container)
 }
 
-const observer = new IntersectionObserver((entries) => {
+const intersection = (entries) => {
     entries
         .filter(isIntersecting)
-        .forEach(loadImage)
-})
-
-export const registerImage = (image) => {
-    observer.observe(image)
+        .forEach(loadImage)    
 }
+const observer = new IntersectionObserver(intersection)
+
+const count = () =>{
+    let shown = 0
+    let append = 0
+    const counting = (appendImages, shownImages) =>{        
+        shown += shownImages
+        append += appendImages
+        console.log('-------------------')
+        console.log(`Imágenes agregadas: ${append}`)
+        console.log(`Imágenes mostradas: ${shown}`)
+        console.log('-------------------')
+    }
+    return counting        
+}
+
+export const registerImage = (image, appendImages) => {
+    observer.observe(image)
+    if(appendImages == 1){
+        reports = count()
+    }
+    reports(1,0)            
+}
+
